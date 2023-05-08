35ad6b663148621b5998f9b1f41df25f30fcce9f
Pontus Wikstrom <pontus.wikstrom.03@gmail.com>
2023-05-08 19:07:13 +0200

    Mindre bös

--- README.md
+++
@@ -1,1 +0,0 @@
-# GUI_final_project
---
+++ iMatApp/src/imat/ProductCard.java
@@ -0,0 +1,11 @@
+package imat;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
+
+public class ProductCard {
+}

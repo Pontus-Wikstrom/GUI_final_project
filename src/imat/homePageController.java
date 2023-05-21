da3f30f0b832c76197513c857ffa1b56fc365bc6
kalimooo <kallewiklund2002@gmail.com>
2023-05-21 12:09:57 +0200

    Update homePageController.java

--- src/imat/homePageController.java
+++ src/imat/homePageController.java
@@ -1,7 +1,32 @@
 package imat;
 
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 
-public class homePageController extends AnchorPane{
-    
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+public class HomePageController extends AnchorPane{
+    @FXML
+    private TextField searchBar;
+    @FXML
+    private FlowPane productListFlowPane;
+
+    public HomePageController() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sökSida.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+        System.out.println("/////");
+        System.out.println(menuBar.getSelectedToggle());
+    }
+
 }

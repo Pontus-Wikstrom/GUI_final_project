2444ed04f5be7122ce0b5e1f134e4d238455ce7f
kalimooo <kallewiklund2002@gmail.com>
2023-05-27 12:33:06 +0200

    change page kinda works

--- src/imat/HomePageController.java
+++ src/imat/HomePageController.java
@@ -4,6 +4,7 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -26,10 +27,14 @@ public class HomePageController extends AnchorPane{
     private AnchorPane flowPaneAnchorPane;
     @FXML
     private ScrollPane homePageScrollPane;
+    @FXML
+    private Text currentSideText;
 
     private HashMap<String, ProductCardController> productCardHashMap;
 
     private final Model model = Model.getInstance();
+
+    private int sideIndex;
 
     public HomePageController(HashMap<String, ProductCardController> productCardHashMap) {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("sökSida.fxml"));
@@ -42,9 +47,10 @@ public class HomePageController extends AnchorPane{
             throw new RuntimeException(exception);
         }
 
-        
-
+    
         this.productCardHashMap = productCardHashMap;
+        sideIndex = 0;
+
         initFlowPane();
         searchBar.setFocusTraversable(false);
 
@@ -72,14 +78,25 @@ public class HomePageController extends AnchorPane{
     public void fillProductListFlowPane() {
         productListFlowPane.getChildren().clear();
 
-        int i = 0;
-        for (Product product : model.getProducts()) {
-            if (i>20) break;
-            
+        int startIndex = sideIndex * 21;
+        int endIndex = startIndex + 20;
+        currentSideText.setText((sideIndex + 1) + "");
+
+        for (int i = startIndex; i <= endIndex; i++) {
+            Product product = model.getProducts().get(i);
             productListFlowPane.getChildren().add(this.productCardHashMap.get(product.getName()));
             
-            i++;
         }
+
+        // for (int i = 0; i<5; i++) {
+
+        // }
+        // for (Product product : model.getProducts()) {
+        //     if (i>20) break;
+            
+        //     productListFlowPane.getChildren().add(this.productCardHashMap.get(product.getName()));
+            
+        // }
     }
 
     private void fillProductListFlowPane(List<Product> products) {
@@ -88,6 +105,18 @@ public class HomePageController extends AnchorPane{
         for (Product product : products) {
             productListFlowPane.getChildren().add(this.productCardHashMap.get(product.getName()));
         }
+    }
+
+    @FXML
+    public void nextPageClick() {
+        if (sideIndex < Math.floor(model.getProducts().size() / 20)) sideIndex++;
+        fillProductListFlowPane();
+    }
+
+    @FXML
+    public void previousPageClick() {
+        if (sideIndex > 0) sideIndex--;
+        fillProductListFlowPane();
     }
 
     @FXML
